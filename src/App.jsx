5a1170f8d8e1c67c086574838321a2063e3e66ee
ebrahimmousa <ebrahim-mousa@hotmail.com>
2024-01-18 14:22:12 +0100

    Added dark mode by @mui

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import Header from "./components/header/header"
-
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+ 
+ 
+ 
 function App() {
-
+  const [theme, colorMode] = useMode();
+ 
   return (
-    <>
-<Header/>
- </>
+ 
+    <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+            <CssBaseline />
+    <Header />
+    </ThemeProvider>
+    </ColorModeContext.Provider>
+ 
+ 
+ 
   )
 }
-
-export default App
+ 
+export default App;
