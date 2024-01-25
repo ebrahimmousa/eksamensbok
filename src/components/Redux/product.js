1e8e93d1c4962d81963c8d68d62f05c730458f0e
ebrahimmousa <ebrahim-mousa@hotmail.com>
2024-01-25 15:56:19 +0100

    Added some url for products

--- src/components/Redux/product.js
+++ src/components/Redux/product.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 // Define a service using a base URL and expected endpoints
 export const productApi = createApi({
   reducerPath: 'productApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:1337/api' }),
+  baseQuery: fetchBaseQuery({ baseUrl: `${import.meta.env.VITE_BASE_URL}/api`}),
   endpoints: (builder) => ({
     getproductByName: builder.query({
       query: (name) => `${name}`,
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -21,114 +21,124 @@ const Main = () => {
   const { data, error, isLoading } = useGetproductByNameQuery(
     "products?populate=*"
   );
-  return (
-    <Container sx={{ mt: 5 }}>
-      <Stack
-        direction={"row"}
-        alignItems={"center"}
-        justifyContent={"space-between"}
-        flexWrap={"wrap"}
-        gap={3}
-      >
-        <Box>
-          <Typography variant="h6">Selected Products</Typography>
-          <Typography fontWeight={300} variant="body1">
-            All our new arrivals in an exclusive brand selection
-          </Typography>
-        </Box>
-        <ToggleButtonGroup
-          color="error"
-          value={alignment}
-          exclusive
-          onChange={handleAlignment}
-          aria-label="text alignment"
-          sx={{
-            ".mui-selected": {
-              border: "1px solid rgba (233, 69, 96, 0.5) !important",
-              color: "#e94560",
-              bgcolor: "initial",
-            },
-          }}
-        >
-          <ToggleButton
-            sx={{ color: theme.palette.text.primary }}
-            className="myButton"
-            value="left"
-            aria-label="left aligned"
-          >
-            All Books{" "}
-          </ToggleButton>
-          <ToggleButton
-            sx={{ mx: "16px !important", color: theme.palette.text.primary }}
-            className="myButton"
-            value="center"
-            aria-label="centered"
-          >
-            Books{" "}
-          </ToggleButton>
-          <ToggleButton
-            sx={{ color: theme.palette.text.primary }}
-            className="myButton"
-            value="right"
-            aria-label="right aligned"
-          >
-            Audio Books{" "}
-          </ToggleButton>
-        </ToggleButtonGroup>
-      </Stack>
-      <Stack
-        direction={"row"}
-        flexWrap={"wrap"}
-        justifyContent={"space-between"}
-      >
-        {["as", "sd", "ff", "sss"].map((item) => {
-          return (
-            <Card
-              key={item}
-              sx={{
-                maxWidth: 333,
-                mt: 6,
-                ":hover .MuiCardMedia-root ": {
-                  scale: "1.1",
-                  transition: "1s",
-                },
-              }}
-            >
-              <CardMedia
-                sx={{ height: 277 }}
-                image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
-                title="green iguana"
-              />
-              <CardContent>
-                <Stack
-                  direction={"row"}
-                  justifyContent={"space-between"}
-                  alignItems={"center"}
-                >
-                  <Typography gutterBottom variant="h6" component="div">
-                    tshirt{" "}
-                  </Typography>
 
-                  <Typography variant="subtitle1" component="p">
-                    1299
+  if (data) {
+    return (
+      <Container sx={{ mt: 5 }}>
+        <Stack
+          direction={"row"}
+          alignItems={"center"}
+          justifyContent={"space-between"}
+          flexWrap={"wrap"}
+          gap={3}
+        >
+          <Box>
+            <Typography variant="h6">Selected Products</Typography>
+            <Typography fontWeight={300} variant="body1">
+              All our new arrivals in an exclusive brand selection
+            </Typography>
+          </Box>
+          <ToggleButtonGroup
+            color="error"
+            value={alignment}
+            exclusive
+            onChange={handleAlignment}
+            aria-label="text alignment"
+            sx={{
+              ".mui-selected": {
+                border: "1px solid rgba (233, 69, 96, 0.5) !important",
+                color: "#e94560",
+                bgcolor: "initial",
+              },
+            }}
+          >
+            <ToggleButton
+              sx={{ color: theme.palette.text.primary }}
+              className="myButton"
+              value="left"
+              aria-label="left aligned"
+            >
+              All Books{" "}
+            </ToggleButton>
+            <ToggleButton
+              sx={{ mx: "16px !important", color: theme.palette.text.primary }}
+              className="myButton"
+              value="center"
+              aria-label="centered"
+            >
+              Books{" "}
+            </ToggleButton>
+            <ToggleButton
+              sx={{ color: theme.palette.text.primary }}
+              className="myButton"
+              value="right"
+              aria-label="right aligned"
+            >
+              Audio Books{" "}
+            </ToggleButton>
+          </ToggleButtonGroup>
+        </Stack>
+        <Stack
+          direction={"row"}
+          flexWrap={"wrap"}
+          justifyContent={"space-between"}
+        >
+          {data.data.map((item) => {
+            return (
+              <Card
+                key={item}
+                sx={{
+                  maxWidth: 333,
+                  mt: 6,
+                  ":hover .MuiCardMedia-root ": {
+                    scale: "1.1",
+                    transition: "1s",
+                  },
+                }}
+              >
+                <CardMedia
+                  sx={{ height: 277 }}
+                  image={`${import.meta.env.VITE_BASE_URL}${
+                    item.attributes.productImage.data[0].attributes.url
+                  }`}
+                  title="green iguana"
+                />
+                <CardContent>
+                  <Stack
+                    direction={"row"}
+                    justifyContent={"space-between"}
+                    alignItems={"center"}
+                  >
+                    <Typography gutterBottom variant="h6" component="div">
+                      {item.attributes.productName}
+                    </Typography>
+
+                    <Typography variant="subtitle1" component="p">
+                      {item.attributes.productPrice}kr
+                    </Typography>
+                  </Stack>
+                  <Typography variant="body2" component="text.secondary">
+                    {item.attributes.description}
                   </Typography>
-                </Stack>
-                <Typography variant="body2" component="text.secondary">
-                  dknskdnsaljdnsaljdnasjldn{" "}
-                </Typography>
-              </CardContent>
-              <CardActions sx={{ justifyContent: "space-between" }}>
-                <Button sx={{ textTransform: "capitalize" }} size="small">
-                  Add to cart
-                </Button>
-                <Rating precision={0.5} name="read-only" value={4.5} readOnly />
-              </CardActions>
-            </Card>
-          );
-        })}
-      </Stack>
-    </Container>
-  );
+                </CardContent>
+                <CardActions sx={{ justifyContent: "space-between" }}>
+                  <Button sx={{ textTransform: "capitalize" }} size="small">
+                    Add to cart
+                  </Button>
+                  <Rating
+                    precision={0.5}
+                    name="read-only"
+                    value={item.attributes.productRating}
+                    readOnly
+                  />
+                </CardActions>
+              </Card>
+            );
+          })}
+        </Stack>
+      </Container>
+    );
+  }
 };
 
 export default Main;
