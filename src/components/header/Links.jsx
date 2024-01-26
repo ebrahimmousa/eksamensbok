ca3984ea47a8872ae441dcea36c6aa5379c53575
ebrahimmousa <ebrahim-mousa@hotmail.com>
2024-01-26 17:24:30 +0100

    Added links styling

--- src/components/header/Links.jsx
+++ src/components/header/Links.jsx
@@ -1,28 +1,12 @@
-import { Box, Paper, Typography } from "@mui/material";
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import "./Links.css";
 
 const Links = ({ title }) => {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        alignItems: "center",
-        position: "relative",
-        ":hover": { cursor: "pointer" },
-      }}
-    >
+    <Box className="link-container">
       <Typography variant="body1">{title}</Typography>
-
-      <Box
-        className="show-when-hover"
-        sx={{
-          position: "absolute",
-          top: "100%",
-          minWidth: "170px",
-          transform: "translateX(-50%)",
-          display: "none",
-          left: "25%",
-        }}
-      ></Box>
+      <Box className="show-when-hover"></Box>
     </Box>
   );
 };
